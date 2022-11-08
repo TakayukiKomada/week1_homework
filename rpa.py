b67147dd7803592aab0975efc84019ff5c757ea2
TakayukiKomada <Takakoma.79@gmail.com>
2022-11-08 10:11:27 +0900

    課題1完成しました

--- rpa.py
+++ rpa.py
@@ -1,5 +1,16 @@
 import webbrowser
+import pyautogui as pa
+import pyperclip
+import time
 
 url = "https://www.google.com"
 
 webbrowser.open(url, new=0, autoraise=True)
+pa.moveTo(660, 450, 1)
+pyperclip.copy("平泉 観光")
+pa.hotkey("ctrl", "v")
+pa.click(x=660, y=450)
+pa.hotkey("enter")
+time.sleep(3)
+screen_shot = pa.screenshot()
+screen_shot.save("test.png")
